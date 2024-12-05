98919b88bdc6481df105b5cf3bd6ba49f9e13b54
Luciano Pulido <lucianonicolaspulido@gmail.com>
2024-12-05 15:45:47 -0300

    Change tab title and delete unnecesary import in router

--- frontend/src/utils/router.jsx
+++ frontend/src/utils/router.jsx
@@ -3,7 +3,6 @@ const RedirectIfLoggedIn = lazy(()=> import("../components/RedirectIfLogin.jsx")
 const ProtectedRoutes = lazy(()=> import("../components/ProtectedRoutes.jsx"))
 const LoginPage = lazy(() => import("../pages/LoginPage.jsx"));
 const HistoriaClinicaPage = lazy(() => import("../pages/HistoriaClinicaPage.jsx"));
-const UnauthorizedPage = lazy(()=> import("../pages/UnauthorizedPage.jsx"))
 
 export const routes = [
     {
